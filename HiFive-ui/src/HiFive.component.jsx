530cd2a8abf344b1f92bcb48939c007563c2948e
Zac Braddy <zacharybraddy@gmail.com>
2016-11-01 23:25:14 +0000

    Updated some styling of the UI app.

--- HiFive-ui/src/HiFive.component.jsx
+++ HiFive-ui/src/HiFive.component.jsx
@@ -3,7 +3,11 @@ import React, { PropTypes } from 'react';
 const HiFive = (props) => (
   <div>
     <div>{props.isHiFived ? 'YEAAAAH WOOOOOH!!!' : 'Hey buddy! Hi 5?!' }</div>
-    <button onClick={props.doHiFive} disabled={props.isHiFived}>Give Hi5</button>
+    <button onClick={props.doHiFive} disabled={props.isHiFived} className="bro-button">
+      <i className={`fa bro-five-icon ${props.isHiFived ? 'fa-hand-rock-o' : 'fa-hand-paper-o'}`} />
+      Give Hi5
+      <i className={`fa bro-five-icon ${props.isHiFived ? 'fa-hand-rock-o' : 'fa-hand-paper-o'}`} />
+    </button>
   </div>
 );
 
